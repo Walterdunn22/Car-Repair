1691ea8df3436251cffbf987cf8f0a508c1add03
Walterdunn22 <151873698+Walterdunn22@users.noreply.github.com>
2023-11-27 00:30:13 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Car Repair Abu Dhabi - Mussafah</title>
+    <link rel="stylesheet" href="style.css">
+
+    <style>
+        body {
+            text-align: center;
+        }
+
+        header, main, footer {
+            margin: 0 auto;
+            width: 80%;
+        }
+
+        h1 {
+            font-size: 24px;
+            font-weight: bold;
+        }
+
+        p {
+            font-size: 16px;
+            line-height: 1.5;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Find Reliable Car Repair in Abu Dhabi - Mussafah</h1>
+    </header>
+    <main>
+        <p>Maintaining your vehicle is crucial for ensuring its smooth operation and longevity. When it comes to <a href="https://alzaabiautocare.com/">Car Repair in Abu Dhabi</a> - Mussafah, trust Al Zaabi Auto Care, your one-stop solution for all your automotive needs.</p>
+        <p>Al Zaabi Auto Care offers a comprehensive range of car repair services, catering to all vehicle makes and models. Their team of experienced and certified technicians utilizes the latest diagnostic tools and techniques to accurately identify and rectify any issues with your car.</p>
+        <p>Whether you require routine maintenance, major repairs, or specialized services, Al Zaabi Auto Care has the expertise to handle it all. They prioritize customer satisfaction and strive to provide transparent and reliable service.</p>
+        <p>Visit their website or contact them directly to schedule an appointment and experience the difference that Al Zaabi Auto Care can make in keeping your vehicle in top condition.</p>
+    </main>
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Al Zaabi Auto Care</p>
+    </footer>
+</body>
+</html>
